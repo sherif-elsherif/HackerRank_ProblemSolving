ada8d2f9e868fc22016d5b5be30ab49cad48d1f9
Sherif ElSherif <55397885+sherif-elsherif@users.noreply.github.com>
2020-03-24 13:12:12 -0400

    https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem

---
+++ BreakingtheRecords.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+
+import os
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

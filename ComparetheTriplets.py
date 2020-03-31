45dcb315d355545c0209cbaf26801ec9b7750b95
Sherif ElSherif <55397885+sherif-elsherif@users.noreply.github.com>
2020-03-31 11:19:50 -0400

    Compare the Triplets Problem

--- ComparetheTriplets.py
+++ ComparetheTriplets.py
@@ -1,1 +1,28 @@
-#"Testing file""
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def compareTriplets(a, b):
+    i = 0
+    score = [0, 0]
+    while i < len(a):
+        if a[i] > b[i]:
+            score[0] += 1
+        elif a[i] < b[i]:
+            score[1] += 1
+        i += 1
+    return score
+
+
+a = list(map(int, input().rstrip().split()))
+
+b = list(map(int, input().rstrip().split()))
+
+result = compareTriplets(a, b)
+
+print(' '.join(map(str, result)))

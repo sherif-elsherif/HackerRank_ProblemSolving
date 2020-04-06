68452c2a9c5f9cdca4bbea827f2de62b10be8a17
Sherif ElSherif <55397885+sherif-elsherif@users.noreply.github.com>
2020-04-06 11:04:18 -0400

    Create BirthdayChocolate.py

---
+++ BirthdayChocolate.py
@@ -0,0 +1,30 @@
+# https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the kangaroo function below.
+def birthday(s, d, m):
+
+
+    return None
+
+
+n = int(input().strip())
+
+s = list(map(int, input().rstrip().split()))
+
+dm = input().rstrip().split()
+
+d = int(dm[0])
+
+m = int(dm[1])
+
+result = birthday(s, d, m)
+
+print(str(result) + '\n')

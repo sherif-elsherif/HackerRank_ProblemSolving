c3c648a1537d578666ea4014bb0ab289f97e29b4
Sherif ElSherif <55397885+sherif-elsherif@users.noreply.github.com>
2020-05-27 08:10:45 -0400

    Update AppendandDelete.py

--- AppendandDelete.py
+++ AppendandDelete.py
@@ -7,6 +7,26 @@
 def appendAndDelete(s, t, k):
     if abs(len(s)-len(t)) > k:
         return 'No'
+    matched_string = ''
+    for i in range(len(t)):
+        if s[i] == t[i]:
+            matched_string = t[0:i+1]
+            continue
+        else:
+            matched_string = t[0:i]
+            break
+    print(matched_string)
+    # Check if you can append the missing values
+    if len(t)-len(matched_string) > k:
+        return 'No'
+    else:
+        return 'Yes'
+    # Start appending the missing values if you'd like to
+    t_index = len(matched_string)
+    while t_index < len(t):
+        matched_string += t[t_index]
+        t_index += 1
+    return matched_string
 
 
 

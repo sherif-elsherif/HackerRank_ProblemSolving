372076220bd15168a051ee4f75757b1c7e51e98b
Sherif ElSherif <55397885+sherif-elsherif@users.noreply.github.com>
2020-05-27 07:43:11 -0400

    Append and Delete Problem

---
+++ AppendandDelete.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/append-and-delete/problem?h_r=next-challenge&h_v=zen
+
+
+
+
+def appendAndDelete(s, t, k):
+    return ""
+
+
+s = input()
+
+t = input()
+
+k = int(input())
+
+result = appendAndDelete(s, t, k)
+
+print(result)
+
+

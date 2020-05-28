55262cb95856c97b6f4c25f748d6d646784b709e
Sherif ElSherif <55397885+sherif-elsherif@users.noreply.github.com>
2020-05-28 07:06:42 -0400

    Update AppendandDelete.py

--- AppendandDelete.py
+++ AppendandDelete.py
@@ -1,14 +1,17 @@
 #!/bin/python3
 # https://www.hackerrank.com/challenges/append-and-delete/problem?h_r=next-challenge&h_v=zen
 
-
-
-
+"""
+abcd
+abcdert
+10
+"""
 def appendAndDelete(s, t, k):
     if abs(len(s)-len(t)) > k:
         return 'No'
     matched_string = ''
-    for i in range(len(t)):
+    min_len = min(len(s), len(t))
+    for i in range(min_len):
         if s[i] == t[i]:
             matched_string = t[0:i+1]
             continue
@@ -17,9 +20,11 @@ def appendAndDelete(s, t, k):
             break
     print(matched_string)
 
-    if len(t) > len(s) and len(t)-len(matched_string) > k:
+    if len(t) > len(s) and len(t)-len(matched_string)+len(s)-len(matched_string) > k:
         return 'No'
-    elif len(t) < len(s):
+    elif len(t) < len(s) and len(s)-len(matched_string)+len(t)-len(matched_string) > k:
+        return 'No'
+    elif len(t) == len(s) and len(s)-len(matched_string)+len(t)-len(matched_string) > k:
         return 'No'
     else:
         return 'Yes'
